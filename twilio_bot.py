b5e92ae554938e8024546d7b8e829247d5d82e89
Fahad Mirza <fahadmirza8@gmail.com>
2021-10-09 22:28:05 -0500

    updated the code :boom:

--- twilio_bot.py
+++ twilio_bot.py
@@ -24,9 +24,65 @@ to_zone = tz.gettz('America/Dallas')
 
 @app.route('/', methods=['POST'])
 def receive_sms():
-    body = request.values.get('Body', None)
+    body = request.values.get('Body', None).lower().strip()
     resp = MessagingResponse()
-    resp.message(body or 'Hello World!')
+
+    if body == 'today':
+        data = requests.get(urls['today']).json()
+        output = "\n"
+        for match in data:
+            output += match['home_team_country'] + ' vs ' + \
+                      match['away_team_country'] + " at " + \
+                      parser.parse(match['datetime']).astimezone(to_zone).strftime('%I:%M %p') + "\n"
+        else:
+            output += "No matches happening today"
+
+    elif body == 'tomorrow':
+        data = requests.get(urls['tomorrow']).json()
+        output = "\n"
+        for match in data:
+            output += match['home_team_country'] + ' vs ' + \
+                      match['away_team_country'] + " at " + \
+                      parser.parse(match['datetime']).astimezone(to_zone).strftime('%I:%M %p') + "\n"
+        else:
+            output += "No matches happening tomorrow"
+
+    elif body.upper() in countries:
+        data = requests.get(urls['country'] + body).json()
+        output = "\n--- Past Matches ---\n"
+        for match in data:
+            if match['status'] == 'completed':
+                output += match['home_team']['country'] + " " + \
+                      str(match['home_team']['goals']) + " vs " + \
+                      match['away_team']['country'] + " " + \
+                      str(match['away_team']['goals']) + "\n"
+
+        output += "\n\n--- Future Matches ---\n"
+        for match in data:
+            if match['status'] == 'future':
+                output += match['home_team']['country'] + " vs " + \
+                          match['away_team']['country'] + " at " + \
+                          parser.parse(match['datetime']).astimezone(to_zone).strftime('%I:%M %p on %d %b') + "\n"
+
+    elif body == 'complete':
+        data = requests.get(urls['group']).json()
+        output = ""
+        for group in data:
+            output += "\n\n--- Group " + group['letter'] + " ---\n"
+            for team in group['ordered_teams']:
+                output += team['country'] + " Pts: " + \
+                      str(team['points']) + "\n"
+    elif body == 'list':
+        output = '\n'.join(countries)
+    else:
+        output = ('Sorry we could not understand your response. '
+                  'You can respond with "today" to get today\'s details, '
+                  '"tomorrow" to get tomorrow\'s details, "complete" to '
+                  'get the group stage standing of teams or '
+                  'you can reply with a country FIFA code (like BRA, ARG) '
+                  'and we will send you the standing of that particular country. '
+                  'For a list of FIFA codes send "list".\n\nHave a great day!')
+    resp.message(output)
     return str(resp)
 
 
