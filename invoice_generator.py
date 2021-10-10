c5b32ae2cca0417473a1de997d8ac91220341578
Fahad Mirza <fahadmirza8@gmail.com>
2021-10-09 22:49:15 -0500

    Strings are converted to double quote.

--- invoice_generator.py
+++ invoice_generator.py
@@ -7,49 +7,50 @@ import io
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def hello_world():
     posted_data = request.get_json() or {}
     today = datetime.today().strftime("%B %d, %Y")
-    invoice_number = 666
+
     default_data = {
-        'duedate': 'August 1, 2222',
-        'from_addr': {
-            'addr1': '54321 Icy Road',
-            'addr2': 'Mount Everest, Nepal 12345',
-            'company_name': '7-Eleven'
+        "duedate": "August 1, 2222",
+        "from_addr": {
+            "addr1": "54321 Icy Road",
+            "addr2": "Mount Everest, Nepal 12345",
+            "company_name": "7-Eleven"
         },
-        'invoice_number': 123,
-        'items': [
+        "invoice_number": 666,
+        "items": [
             {
-                'charge': 300.0,
-                'title': 'Website Design'
+                "charge": 300.0,
+                "title": "Website Design"
             },
             {
-                'charge': 750.0,
-                'title': 'Circuit Design'
+                "charge": 750.0,
+                "title": "Circuit Design"
             },
             {
-                'charge': 100.0,
-                'title': 'PCB Manufacturing'
+                "charge": 100.0,
+                "title": "PCB Manufacturing"
             }
         ],
-        'to_addr': {
-            'company_name': 'Hitman47',
-            'person_email': 'john@doe.com',
-            'person_name': 'John Doe'
+        "to_addr": {
+            "company_name": "Hitman47",
+            "person_email": "john@doe.com",
+            "person_name": "John Doe"
         }
     }
 
-    duedate = posted_data.get('duedate', default_data['duedate'])
-    from_addr = posted_data.get('from_addr', default_data['from_addr'])
-    to_addr = posted_data.get('to_addr', default_data['to_addr'])
-    invoice_number = posted_data.get('invoice_number', default_data['invoice_number'])
-    items = posted_data.get('items', default_data['items'])
+    duedate = posted_data.get("duedate", default_data["duedate"])
+    from_addr = posted_data.get("from_addr", default_data["from_addr"])
+    to_addr = posted_data.get("to_addr", default_data["to_addr"])
+    invoice_number = posted_data.get("invoice_number", default_data["invoice_number"])
+    items = posted_data.get("items", default_data["items"])
 
-    total = sum([i['charge'] for i in items])
+    total = sum([i["charge"] for i in items])
 
-    rendered = render_template('invoice.html',
+    rendered = render_template("invoice.html",
                                date=today,
                                from_addr=from_addr,
                                to_addr=to_addr,
@@ -58,17 +59,17 @@ def hello_world():
                                invoice_number=invoice_number,
                                duedate=duedate)
 
-    with open('invoice.html', 'w') as html_file:
+    with open("invoice.html", "w") as html_file:
         html_file.write(rendered)
         html_file.flush()
-        pdf_binary = pdfkit.from_file("invoice.html", b'')
+        pdf_binary = pdfkit.from_file("invoice.html", b"")
 
     if os.path.exists("invoice.html"):
         os.remove("invoice.html")
 
-    return send_file(io.BytesIO(pdf_binary), download_name='invoice.pdf')
+    return send_file(io.BytesIO(pdf_binary), download_name="invoice.pdf")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host="0.0.0.0", port=port)
--- web_scraper.py
+++ web_scraper.py
@@ -1,39 +1,39 @@
 import requests
 import lxml.html
 
-html = requests.get('https://store.steampowered.com/explore/new/')
+html = requests.get("https://store.steampowered.com/explore/new/")
 doc = lxml.html.fromstring(html.content)
 
-new_releases = doc.xpath('//div[@id="tab_newreleases_content"]')[0]
-titles = new_releases.xpath('.//div[@class="tab_item_name"]/text()')
-prices = new_releases.xpath('.//div[@class="discount_final_price"]/text()')
-tags_divs = new_releases.xpath('.//div[@class="tab_item_top_tags"]')
+new_releases = doc.xpath("//div[@id='tab_newreleases_content']")[0]
+titles = new_releases.xpath(".//div[@class='tab_item_name']/text()")
+prices = new_releases.xpath(".//div[@class='discount_final_price']/text()")
+tags_divs = new_releases.xpath(".//div[@class='tab_item_top_tags']")
 tags = []
 
 for div in tags_divs:
     tags.append(div.text_content())
 
-tags = [tag.split(', ') for tag in tags]
+tags = [tag.split(", ") for tag in tags]
 
 
-platforms_div = new_releases.xpath('.//div[@class="tab_item_details"]')
+platforms_div = new_releases.xpath(".//div[@class='tab_item_details']")
 total_platforms = []
 
 for game in platforms_div:
-    temp = game.xpath('.//span[contains(@class, "platform_img")]')
+    temp = game.xpath(".//span[contains(@class, 'platform_img')]")
 
-    platforms = [t.get('class').split(' ')[-1] for t in temp]
-    if 'hmd_separator' in platforms:
-        platforms.remove('hmd_separator')
+    platforms = [t.get("class").split(" ")[-1] for t in temp]
+    if "hmd_separator" in platforms:
+        platforms.remove("hmd_separator")
     total_platforms.append(platforms)
 
 output = []
 for info in zip(titles, prices, tags, total_platforms):
     resp = {
-        'title': info[0],
-        'price': info[1],
-        'tags': info[2],
-        'platforms': info[3]
+        "title": info[0],
+        "price": info[1],
+        "tags": info[2],
+        "platforms": info[3]
     }
 
     output.append(resp)
