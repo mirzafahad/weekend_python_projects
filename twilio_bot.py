dc6011bff66c3779743abe272eb38f669a8d3cf1
Fahad Mirza <fahadmirza8@gmail.com>
2021-10-09 22:38:41 -0500

    updated the code :boom:

--- twilio_bot.py
+++ twilio_bot.py
@@ -3,6 +3,7 @@ from flask import Flask, request
 import requests
 from dateutil import parser, tz
 from twilio.twiml.messaging_response import MessagingResponse
+import pyjokes
 
 urls = {
     'group': 'https://worldcup.sfg.io/teams/group_results',
@@ -27,61 +28,20 @@ def receive_sms():
     body = request.values.get('Body', None).lower().strip()
     resp = MessagingResponse()
 
-    if body == 'today':
-        data = requests.get(urls['today']).json()
-        output = "\n"
-        for match in data:
-            output += match['home_team_country'] + ' vs ' + \
-                      match['away_team_country'] + " at " + \
-                      parser.parse(match['datetime']).astimezone(to_zone).strftime('%I:%M %p') + "\n"
-        else:
-            output += "No matches happening today"
+    if body == 'joke':
+        output = pyjokes.get_joke(language="en", category="all")
 
-    elif body == 'tomorrow':
-        data = requests.get(urls['tomorrow']).json()
-        output = "\n"
-        for match in data:
-            output += match['home_team_country'] + ' vs ' + \
-                      match['away_team_country'] + " at " + \
-                      parser.parse(match['datetime']).astimezone(to_zone).strftime('%I:%M %p') + "\n"
-        else:
-            output += "No matches happening tomorrow"
+    elif body == 'joke neutral':
+        output = pyjokes.get_joke(language="en", category="neutral")
 
-    elif body.upper() in countries:
-        data = requests.get(urls['country'] + body).json()
-        output = "\n--- Past Matches ---\n"
-        for match in data:
-            if match['status'] == 'completed':
-                output += match['home_team']['country'] + " " + \
-                      str(match['home_team']['goals']) + " vs " + \
-                      match['away_team']['country'] + " " + \
-                      str(match['away_team']['goals']) + "\n"
+    elif body == 'joke twister':
+        output = pyjokes.get_joke(language="en", category="twister")
 
-        output += "\n\n--- Future Matches ---\n"
-        for match in data:
-            if match['status'] == 'future':
-                output += match['home_team']['country'] + " vs " + \
-                          match['away_team']['country'] + " at " + \
-                          parser.parse(match['datetime']).astimezone(to_zone).strftime('%I:%M %p on %d %b') + "\n"
-
-    elif body == 'complete':
-        data = requests.get(urls['group']).json()
-        output = ""
-        for group in data:
-            output += "\n\n--- Group " + group['letter'] + " ---\n"
-            for team in group['ordered_teams']:
-                output += team['country'] + " Pts: " + \
-                      str(team['points']) + "\n"
-    elif body == 'list':
-        output = '\n'.join(countries)
     else:
         output = ('Sorry we could not understand your response. '
-                  'You can respond with "today" to get today\'s details, '
-                  '"tomorrow" to get tomorrow\'s details, "complete" to '
-                  'get the group stage standing of teams or '
-                  'you can reply with a country FIFA code (like BRA, ARG) '
-                  'and we will send you the standing of that particular country. '
-                  'For a list of FIFA codes send "list".\n\nHave a great day!')
+                  'You can respond with "joke" to get a random joke, '
+                  '"joke neutral" to get a neutral joke, "joke twister" to '
+                  'get the a tongue-twister joke.\n\nHave a great day!')
     resp.message(output)
     return str(resp)
 
